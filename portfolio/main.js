788f1a8aab7bf6824bd0ccdc94685b02f8182e70
Karishma <karishma@beanbyte.in>
2023-09-03 23:28:35 +0530

    portfolio init

---
+++ portfolio/main.js
@@ -0,0 +1,9 @@
+// alert("hello");
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","PHP Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+});
